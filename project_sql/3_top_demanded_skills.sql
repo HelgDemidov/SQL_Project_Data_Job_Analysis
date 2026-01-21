28570c1f0fff75703cf951960156948f650c248d
HelgDemidov <helg.demidov88@gmail.com>
2026-01-21 05:04:11 +0100

    Question 3: solution finalized

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,8 +1,10 @@
 /*
-Question 4: Identify most in-demand skills for Data Engineer
+Question 3: Identify most in-demand skills for Data Engineer (DE)
     - Join job postings to inner join table similarly to query 2
     - Identify the top-5 in-demand skills for Data Engineer job (including Senior DE, Chief DE, Lead DE, etc.)
     - Cover all job postings (whichever has skills associated with it in the dataset)
+    - ADDITIONAL: identify what the SHARE (percentile, %) of each of the top-5 skills most often required for Data Engineers
+    is in relation to ALL Data Engineer job postings
 */
 
 WITH skill_stats AS (
@@ -14,7 +16,7 @@ WITH skill_stats AS (
         INNER JOIN skills_dim AS s ON
             sj.skill_id = s.skill_id
         INNER JOIN job_postings_fact AS p ON
-            sj.job_id = p.job_id
+            sj.job_id = p.job_id 
         WHERE 
             job_title_short LIKE '%Data Engineer%'
          -- AND p.job_work_from_home = true -- optional parameter: remote jobs
@@ -26,24 +28,23 @@ WITH skill_stats AS (
     LIMIT 5
 ),
 
-    job_stats AS (
+    de_job_stats AS (
     SELECT 
-        COUNT(DISTINCT job_id) AS total_jobs
+        COUNT(DISTINCT job_id) AS total_de_jobs
     FROM job_postings_fact
-    WHERE 
-        job_title_short LIKE '%Data Engineer%'
+    WHERE job_title_short LIKE '%Data Engineer%'
      -- AND job_work_from_home = true -- optional parameter: remote jobs
      -- AND salary_year_avg > 80000 -- optional parameter: Medium+ paid jobs
     )
 
 SELECT 
     ss.*,
-    ROUND((ss.related_jobs::numeric / js.total_jobs) * 100, 3) || '%' AS skill_share
+    ROUND((ss.related_jobs::numeric / djs.total_de_jobs) * 100, 3) || '%' AS skill_share
 FROM 
-    skill_stats ss CROSS JOIN job_stats js
+    skill_stats ss CROSS JOIN de_job_stats djs
 GROUP BY 
     ss.skill_name,
     ss.skill_type, 
     ss.related_jobs,
-    js.total_jobs
-ORDER BY ROUND((ss.related_jobs::numeric / js.total_jobs) * 100, 3) DESC;
+    djs.total_de_jobs
+ORDER BY ROUND((ss.related_jobs::numeric / djs.total_de_jobs) * 100, 3) DESC;
